a5cc036e02790115e484ecb91fae8a2c71ee37e3
rodapunk <estevanmachry@gmail.com>
2024-01-14 15:33:49 -0300

    Product

---
+++ src/main/java/com/ecm/web_services/repositories/ProductRepository.java
@@ -0,0 +1,18 @@
+package com.ecm.web_services.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ecm.web_services.entities.Product;
+
+/*
+ * @Repository is optional because ProductRepository extends
+ * JpaRepository which is already registered as a Spring @Component! 
+ */
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+	/*
+	 * JpaRepository already has its implementation and there is no
+	 * need to implement any methods here!
+	 */
+}
