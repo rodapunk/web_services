59f62ac98c001ce651369056090c4199e969b8c6
rodapunk <estevanmachry@gmail.com>
2024-01-14 20:34:58 -0300

    OrderItem, many-to-many association with extra attributes PART 1

--- src/main/java/com/ecm/web_services/entities/Product.java
+++ src/main/java/com/ecm/web_services/entities/Product.java
@@ -29,8 +29,8 @@ public class Product implements Serializable {
 	
 	@ManyToMany
 	@JoinTable(name = "tb_product_category",
-	joinColumns = @JoinColumn(name = "product_id"),
-	inverseJoinColumns = @JoinColumn(name = "category_id"))
+		joinColumns = @JoinColumn(name = "product_id"),
+		inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
 	
 	public Product() {
