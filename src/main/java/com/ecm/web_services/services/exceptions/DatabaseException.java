58ad25b9c4a5315057ea780f4e03f8396c0205a9
rodapunk <estevanmachry@gmail.com>
2024-01-16 21:36:21 -0300

    Exception handling - delete

---
+++ src/main/java/com/ecm/web_services/services/exceptions/DatabaseException.java
@@ -0,0 +1,9 @@
+package com.ecm.web_services.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public DatabaseException(String msg) {
+		super(msg);
+	}
+}
